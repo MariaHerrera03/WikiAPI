132040152fa87a53e08eef5e8d17a8cdcaf45929
María F. Herrera G <mariaf.herrera@outlook.com>
2023-02-16 18:20:56 -0500

    V0.1 First commit

---
+++ src/services/workoutService.js
@@ -0,0 +1,19 @@
+const getAllWorkouts = () => {
+    return; 
+};
+
+const getOneWorkout = () => {
+    return; 
+};
+
+const createNewWorkout = () => {
+    return; 
+};
+
+const updateOneWorkout = () => {
+    return; 
+};
+
+const deleteOneWorkout = () => {
+    return; 
+};
